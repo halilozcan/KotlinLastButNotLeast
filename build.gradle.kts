6eab320f0014a8b0dc72ca78f1efaaeb39d11482
Halil ÖZCAN <mail.halilozcan@gmail.com>
2024-05-31 13:54:07 +0300

    first commit with love <3

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
