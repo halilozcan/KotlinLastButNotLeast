fd887dc69d7e1a64224237c7f59f2c949d8979c6
Halil ÖZCAN <mail.halilozcan@gmail.com>
2024-06-02 22:54:38 +0300

    refactor exceptions

--- src/main/kotlin/7-Exceptions.kt
+++ src/main/kotlin/7-Exceptions.kt
@@ -1,46 +1,33 @@
 package org.example
 
+/**
+ * Kotlin de Java da bulunan checked exceptions yoktur
+ * Appendable append(CharSequence csq) throws IOException;
+ * yani bir method tanımlaması yaparken bunun aynı zamanda exception
+ * fırlatacağını belirtemezsiniz.
+ */
+
+/**
+ * Nothing değer döndürülmeyen ve değerin asla olmadığını belirtmek için
+ * kullanılan bir sınıftır. Bir exception fırlatılması ifadesinin sonucu
+ * Nothing dir. Bu tip hiç bir zaman erişilmeyecek code konumlarını işaretlemek
+ * için kullanılır. Nothing final bir classtır. Constructor ı private dır.
+ * Genellikle sadece exception fırlatan methodlar için kullanılır.
+ * Üst sınıf Any dir.
+ */
+
 fun main() {
-    /**
-     * Kotlin de Java da bulunan checked exceptions yoktur
-     * Appendable append(CharSequence csq) throws IOException;
-     * yani bir method tanımlaması yaparken bunun aynı zamanda exception
-     * fırlatacağını belirtemezsiniz.
-     */
-
-    /**
-     * Nothing değer döndürülmeyen ve değerin asla olmadığını belirtmek için
-     * kullanılan bir sınıftır. Bir exception fırlatılması ifadesinin sonucu
-     * Nothing dir. Bu tip hiç bir zaman erişilmeyecek code konumlarını işaretlemek
-     * için kullanılır. Nothing final bir classtır. Constructor ı private dır.
-     * Genellikle sadece exception fırlatan methodlar için kullanılır.
-     * Üst sınıf Any dir.
-     */
-
     val name: String? = null
 
     val length = name?.length ?: error("name can not be null")
     println(length)
-
-    /**
-     * Any bütün sınıfların atasıdır. equals, hashcode ve toString methodlarına
-     * sahiptir. Bu methodlar opendır ve kalıtım alan sınıflar tarafından override
-     * edilebilir. Nesnesi oluşturulabilir.
-     * Javadaki Object sınıfı gibi düşünülebilir.
-     */
-
-    /**
-     * Unit object classtır. Kalıtım alınamaz ya da nesnesi oluşturulamaz.
-     * Javadaki void gibi düşünülebilir. Üst sınıfı Any dir ve override edilmiş
-     * toString methoduna sahiptir.
-     */
 }
 
 fun error(message: String): Nothing {
     throw Exception(message)
 }
 
-class MathOperation(var number1: Int, var number2: Int, var number3: Int) {
+class MathOperation(private var number1: Int, private var number2: Int, private var number3: Int) {
     private fun minusTwoInteger(number1: Int, number2: Int): Int {
         return number1 - number2
     }
@@ -53,11 +40,5 @@ class MathOperation(var number1: Int, var number2: Int, var number3: Int) {
     @Throws(ArithmeticException::class)
     fun doOperation() {
         number1 / minusTwoInteger(number2, number3)
-    }
-
-    fun closeOperation() {
-        number1 = 0
-        number2 = 0
-        number3 = 0
     }
 }
